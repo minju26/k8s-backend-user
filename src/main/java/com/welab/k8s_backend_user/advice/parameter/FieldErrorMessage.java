bb8ce363b18dced151a572ab7189342dd455f3b7
minju26 <mjalswn26@gmail.com>
2025-06-22 17:15:15 +0900

    feat: 공통 API 응답 및 예외 처리 구조 추가

---
+++ src/main/java/com/welab/k8s_backend_user/advice/parameter/FieldErrorMessage.java
@@ -0,0 +1,16 @@
+package com.welab.k8s_backend_user.advice.parameter;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class FieldErrorMessage {
+    private String field;
+    private String message;
+
+    @Builder
+    public FieldErrorMessage(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/welab/k8s_backend_user/advice/parameter/FieldErrorMessageCollection.java
@@ -0,0 +1,42 @@
+package com.welab.k8s_backend_user.advice.parameter;
+
+import lombok.Data;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class FieldErrorMessageCollection {
+
+    private List<FieldErrorMessage> errors = new ArrayList<>();
+
+    FieldErrorMessageCollection(List<FieldError> fieldErrors, List<ObjectError> globalErrors) {
+        errors.addAll(getFieldErrors(fieldErrors));
+        errors.addAll(getObjectErrors(globalErrors));
+    }
+
+    public FieldErrorMessageCollection(List<FieldErrorMessage> errors) {
+        this.errors = errors;
+    }
+
+    private List<FieldErrorMessage> getFieldErrors(List<FieldError> fieldErrors) {
+        return fieldErrors.stream()
+                .map(f -> FieldErrorMessage.builder()
+                        .field(f.getField())
+                        .message(f.getDefaultMessage())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private List<FieldErrorMessage> getObjectErrors(List<ObjectError> globalErrors) {
+        return globalErrors.stream()
+                .map(o -> FieldErrorMessage.builder()
+                        .field(o.getObjectName())
+                        .message(o.getDefaultMessage())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
